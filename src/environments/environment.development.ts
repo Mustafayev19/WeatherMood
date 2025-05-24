c42e7003976fa45c7a4cdc8c390506e3341a770a
Mustafayev19 <azemustafayev19@gmail.com>
2025-05-24 19:23:33 +0400

    debug

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    weatherApiUrl: 'http://api.weatherapi.com/v1/current.json', // Sonda '?' olmadan
+    weatherApiUrl: 'https://api.weatherapi.com/v1/current.json', // Sonda '?' olmadan
     weatherApiKey: '2005996eead74763a9b103614230505' // Yalnız açarın özü, '&key=' olmadan
 };
